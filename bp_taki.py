61a7c2c5bafb52b35427fb283a692d05404d26ff
adielas <adiel.ashrov@gmail.com>
2024-11-24 07:56:24 +0200

    Switch to new BP package interface

--- bp_taki.py
+++ bp_taki.py
@@ -1,4 +1,4 @@
-from bppy import *
+import bppy as bp
 import random
 
 random.seed(10)
@@ -10,26 +10,26 @@ def create_and_shuffle_cards():
     for color in colors:
         for value in values:
             card_event_name= "card_" + value + "_" + color
-            all_cards.append(BEvent(card_event_name))
+            all_cards.append(bp.BEvent(card_event_name))
 
     random.shuffle(all_cards)
     return all_cards
 
-@b_thread
+@bp.thread
 def deal_cards(num_of_players=2, num_of_cards=2):
     cards = create_and_shuffle_cards()
     for i in range(num_of_players):
         for j in range(num_of_cards):
             card_event = cards.pop()
-            player_card_event = BEvent("p_" + str(i) + "_" + card_event.name)
-            yield {request: player_card_event}
+            player_card_event = bp.BEvent("p_" + str(i) + "_" + card_event.name)
+            yield bp.sync(request= player_card_event)
 
-    yield {request: BEvent("finished_dealing_cards")}
+    yield bp.sync(request= bp.BEvent("finished_dealing_cards"))
 
 def init_b_program():
-    b_program = BProgram(bthreads=[deal_cards()],
-                         event_selection_strategy=SimpleEventSelectionStrategy(),
-                         listener=PrintBProgramRunnerListener())
+    b_program = bp.BProgram(bthreads=[deal_cards()],
+                         event_selection_strategy=bp.SimpleEventSelectionStrategy(),
+                         listener=bp.PrintBProgramRunnerListener())
     return b_program
 
 if __name__ == "__main__":
