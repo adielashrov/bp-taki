2a54618995b538aa39534fbe79d0057c4bafe60f
adielas <adiel.ashrov@gmail.com>
2026-04-10 09:45:33 +0300

    Develop an optimal strategy in python for TAKI - first trial #132

--- python_taki_api/__init__.py
+++ python_taki_api/__init__.py
@@ -1,9 +1,11 @@
 from .python_agent import PythonAgent
+from .strategy_agent import TakiStrategyAgent
 from .taki_agent import TakiAgent
 from .taki_game import TakiGame
 
 __all__ = [
     "PythonAgent",
+    "TakiStrategyAgent",
     "TakiAgent",
     "TakiGame",
 ]
